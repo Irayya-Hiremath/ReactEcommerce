4c78880aca53c6652fffe0af249aafbd1f36b10b
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-06-16 23:57:19 +0530

    components upated

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from "react"
+import React, { useState,useEffect } from "react";
+import data from "./data";
+import {Container} from "react-bootstrap";
+import {Link} from 'react-router-dom';
 
 const Product=(props) =>{
-        const[product,setProduct]=useState({}):
+        const[product,setProduct]=useState({});
         useEffect(()=>{
+            const newData=data.find((d)=>d._id=props.match.params.id)
+    
             
-        })
+        },[]);
     return(
         <div> 
-          
+          <Container>
+              <Link className="btn btn-light my-3" to="/">
+                  Go Back
+              </Link>
+              <Row>
+                  <Col md={3}>
+                      <ListGroup varients="flusj">
+                          <ListGroup.Item>
+                              <h3>{product.name}</h3>
+                          </ListGroup.Item>
+                      </ListGroup>
+
+                  </Col>
+              </Row>
+          </Container>
         </div>
     )
 }
